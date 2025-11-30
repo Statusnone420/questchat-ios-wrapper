1847b8a1d5490b61e0a6c6003e3824ed120191d1
Statusnone420 <anthonyga88@icloud.com>
2025-11-30 14:30:05 -0500

    Add initial SwiftUI QuestChat App

---
+++ QuestChat/QuestChat/QuestChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestChatApp.swift
+//  QuestChat
+//
+//  Created by Anthony Gagliardo on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct QuestChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
